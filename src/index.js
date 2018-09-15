7d35bb4b5cc4b7c4bd7052bd2d46caa393286d25
alexnaidovich <32612122+alexnaidovich@users.noreply.github.com>
2018-09-15 20:24:22 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param p - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(p = []) {
+    let num = 0;
+
+    p.map((x, i) => {
+        const shift = 1;
+        let y, z;
+
+        if (x - shift !== i) {
+            y = p[x - shift];
+            if (p[y - shift] !== y) {
+                z = p[y - shift];
+                if (p[z - shift] !== z && i === z - shift) {
+                    num += 1;
+                }
+            }
+        }
+    });
+    return num / 3;
 };
